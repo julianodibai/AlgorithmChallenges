565482e1d25806014e86b6fb4421455fb00054a7
Juliano Dibai <81191797+julianodibai@users.noreply.github.com>
2022-02-04 17:26:54 -0300

    FizzBuzz Entrevisa de emprego

---
+++ C#/Interview Solutions/FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using static System.Console;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<int> colecao = new List<int>();
+        bool InserirNovaLinha = false;
+
+        for (int i = 1; i <= 100; i++)
+        {
+            colecao.Add(i);
+        }
+
+        foreach (int item in colecao)
+        {
+            if(item % 3 == 0)
+            {
+                Write("Fizz");
+                InserirNovaLinha = true;
+            }
+
+            if(item % 5 == 0)
+            {
+                Write("Buzz");
+                InserirNovaLinha = true;
+            }
+
+            if(InserirNovaLinha)
+            {
+                Write(Environment.NewLine);
+            }
+        }
+
+    }
+}
---
+++ C#/Interview Solutions/FizzBuzz/README.md
@@ -0,0 +1,4 @@
+Você tem uma coleção de números inteiros, de 1 a 100. Eu quero que você percorra esta coleção. 
+Para cada número encontrado que é divisível por 3, imprima a palavra "Fizz". 
+Para cada número que é divisível por 5, imprima a palavra "Buzz". Para cada número que é divisível por 3 E 5,
+ imprima a palavra "FizzBuzz".
