369e443b8b55454adc91f6fec3d9820090783e95
Juliano Dibai <81191797+julianodibai@users.noreply.github.com>
2022-02-17 22:38:39 -0300

    Dio 2.0

---
+++ C#/Interview Solutions/Soma de Pares Consecutivos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Console;
+
+namespace DIO2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                int D = int.Parse(ReadLine());
+                int armazemD = D;
+                int soma = 0;
+
+                if(D % 2 == 0)
+                {
+                    soma += D;
+                }
+                else
+                {
+                    armazemD = D+1;
+                    soma += armazemD;
+                }
+
+                for (int i = 1; i < 5; i++)
+                {
+                    armazemD += 2;
+                    soma += armazemD;
+                }
+
+                if(D == 0)
+                    break;
+
+                WriteLine(soma);
+                soma = 0;    
+            }
+
+        }
+    }
+}
