a987c4f9a422557089199b60c48f7f26fa5c5772
Juliano Dibai <81191797+julianodibai@users.noreply.github.com>
2022-02-17 21:58:31 -0300

    Desafios da DIO

---
+++ C#/Interview Solutions/Quadrado e ao Cubo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                WriteLine($"{i} {i*i} {i*i*i}");
+            }
+
+        }
+    }
+}
